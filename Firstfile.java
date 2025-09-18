e51e297e608ec09089c626c2631ab6c04f252d9f
saleemashaik1234 <saleemashaik666@gmail.com>
2025-09-18 17:41:53 +0530

    Create firstfile.java

---
+++ Firstfile.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hi, World" to the terminal window.
+        System.out.println("Hi, World");
+    }
+}
